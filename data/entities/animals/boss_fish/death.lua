b6edb6953fcd1187cbb89a3c79a789c78edaf36c
Mir Sole <zburn101@gmail.com>
2022-01-23 21:40:21 +1100

    leviathan changes

---
+++ data/entities/animals/boss_fish/death.lua
@@ -0,0 +1,16 @@
+dofile_once("data/scripts/lib/utilities.lua")
+
+function death( damage_type_bit_field, damage_message, entity_thats_responsible, drop_items )
+	local entity_id = GetUpdatedEntityID()
+	local x, y = EntityGetTransform( entity_id )
+	
+	ConvertMaterialEverywhere( CellFactory_GetType( "water" ), CellFactory_GetType( "smoke" ) )
+	ConvertMaterialEverywhere( CellFactory_GetType( "lava" ), CellFactory_GetType( "water" ) )
+	EntityLoad( "data/entities/items/pickup/heart_fullhp.xml",  x + 32, y )
+	EntityLoad( "data/entities/items/pickup/chest_random_super.xml",  x - 32, y )
+	EntityLoad( "data/entities/buildings/teleport_teleroom.xml",  x, y )
+	EntityLoad("mods/mould_n/data/entities/items/fishhook.xml", x, y-20)
+	
+	GameAddFlagRun( "miniboss_fish" )
+	AddFlagPersistent( "miniboss_fish" )
+end
