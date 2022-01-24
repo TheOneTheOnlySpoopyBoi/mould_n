69c40d2826e3d1e43cc7efe7a6275b9849e5d185
Mir Sole <zburn101@gmail.com>
2022-01-24 13:12:32 +1100

    hi

--- data/entities/items/hiisi_glue_gun_setup.lua
+++ data/entities/items/hiisi_glue_gun_setup.lua
@@ -3,5 +3,5 @@ dofile("data/scripts/gun/procedural/gun_action_utils.lua")
 
 local entity_id = GetUpdatedEntityID()
 
-AddGunActionPermanent( entity_id, "GUE_SHOT" )
+AddGunActionPermanent( entity_id, "GLUE_SHOT" )
 AddGunActionPermanent( entity_id, "LIFETIME" )
