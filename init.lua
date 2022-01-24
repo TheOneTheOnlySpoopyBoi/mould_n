6a4fb1c0634b13d45d097ae52dc68795ef89d643
Mir Sole <zburn101@gmail.com>
2022-01-24 14:28:26 +1100

    r

--- init.lua
+++ init.lua
@@ -114,6 +114,16 @@ xml:add_child(nxml.parse([[
 ]]))
 ModTextFileSetContent("data/entities/animals/scavenger_heal.xml", tostring(xml))
 
+local nxml = dofile_once("mods/mould_n/lib/nxml.lua")
+local xml = nxml.parse(ModTextFileGetContent("data/entities/animals/boss_centipede/boss_centipede.xml"))
+xml:add_child(nxml.parse([[
+	<LuaComponent
+		script_death="mods/mould_n/files/scripts/boss_kolmi_death.lua"
+		>
+	</LuaComponent>
+]]))
+ModTextFileSetContent("data/entities/animals/boss_centipede/boss_centipede.xml", tostring(xml))
+
 function OnModInit()
 	print("Mod - OnModInit()")
 end
