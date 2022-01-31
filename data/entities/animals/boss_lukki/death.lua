4be9b7c20b2571b67ab14f82487ed967f10985b3
Mir Sole <zburn101@gmail.com>
2022-01-31 20:02:53 +1100

    floppa xd

---
+++ data/entities/animals/boss_lukki/death.lua
@@ -0,0 +1,43 @@
+dofile_once("data/scripts/lib/utilities.lua")
+
+function death( damage_type_bit_field, damage_message, entity_thats_responsible, drop_items )
+	local entity_id = GetUpdatedEntityID()
+	local x, y = EntityGetTransform( entity_id )
+	
+	EntityLoad( "data/entities/items/pickup/heart_fullhp.xml",  x , y )
+
+	EntityLoad( "data/entities/items/pickup/goldnugget_1000.xml",  x, y )
+	EntityLoad( "data/entities/items/pickup/goldnugget_1000.xml",  x, y )
+	EntityLoad( "data/entities/items/pickup/goldnugget_1000.xml",  x, y )
+	EntityLoad( "data/entities/items/pickup/goldnugget_1000.xml",  x, y )
+	EntityLoad( "data/entities/items/pickup/goldnugget_1000.xml",  x, y )
+
+	EntityLoad("mods/mould_n/data/entities/items/toothy_spider_wand.xml", x, y+20)
+	--EntityLoad("mods/mould_n/data/entities/items/toothy_spider_wand.xml", x, y+20) --wand (when i make it) MAYBE A BOW?
+
+	local spelldrop = math.random(1, 9)
+
+	--{ "ALPHA", "OMEGA", "GAMMA", "MU", "RESET", "ZETA", "PHI", "TAU", "SIGMA" }
+
+	if spelldrop == 1 then
+		CreateItemActionEntity( "ALPHA", x, y )
+	elseif spelldrop == 2 then
+		CreateItemActionEntity( "OMEGA", x, y )
+	elseif spelldrop == 3 then
+		CreateItemActionEntity( "GAMMA", x, y )
+	elseif spelldrop == 4 then
+		CreateItemActionEntity( "MU", x, y )
+	elseif spelldrop == 5 then
+		CreateItemActionEntity( "RESET", x, y )
+	elseif spelldrop == 6 then
+		CreateItemActionEntity( "ZETA", x, y )
+	elseif spelldrop == 7 then
+		CreateItemActionEntity( "PHI", x, y )
+	elseif spelldrop == 8 then
+		CreateItemActionEntity( "TAU", x, y )
+	elseif spelldrop == 2 then
+		CreateItemActionEntity( "SIGMA", x, y )
+	end
+
+
+end
--- data/entities/items/sandstorm_sceptre.lua
+++ data/entities/items/sandstorm_sceptre.lua
@@ -3,6 +3,7 @@ dofile("data/scripts/gun/procedural/gun_action_utils.lua")
 
 local entity_id = GetUpdatedEntityID()
 
-AddGunActionPermanent( entity_id, "BURNING_GAZE" )
+AddGunAction( entity_id, "BURNING_GAZE" )
+
 AddGunActionPermanent( entity_id, "LIFETIME" )
 AddGunActionPermanent( entity_id, "HOMING" )
