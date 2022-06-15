a655e5f1f6b271d9aa4d30653c0ede83cfe3462e
Mir Sole <zburn101@gmail.com>
2022-06-15 21:55:21 +1000

    push

--- data/scripts/biomes/monkcity.lua
+++ data/scripts/biomes/monkcity.lua
@@ -1,35 +1,27 @@
--- default biome functions that get called if we can't find a a specific biome that works for us
 CHEST_LEVEL = 3
 dofile_once("data/scripts/director_helpers.lua")
-dofile_once("data/scripts/director_helpers_design.lua")
 dofile_once("data/scripts/biome_scripts.lua")
-dofile_once("data/scripts/biome_modifiers.lua")
-dofile( "data/scripts/items/generate_shop_item.lua" )
 
 RegisterSpawnFunction( 0xffffeedd, "init" )
-RegisterSpawnFunction( 0xffb4a00a, "spawn_monks" )
+RegisterSpawnFunction( 0xff528900, "spawn_monks" ) --0xffffdd35
 
------------- small enemies -------------------------------
 
-g_enemies =
+g_monks =
 {
 	total_prob = 0,
 
-	--[[{
+	{
 		prob   		= 1,
 		min_count	= 1,
 		max_count	= 1,    
 		entity 	= "data/entities/animals/monk.xml"
-	}]]--
+	},
 }
 
--- actual functions that get called from the wang generator
-
 function spawn_monks(x, y)
-	--spawn(g_enemies, x, y)
+	spawn(g_monks,x,y)
 end
 
-
-function init( x, y, w, h ) 
-
+function init( x, y, w, h )
+	--LoadPixelScene( "data/biome_impl/coral_reef_shrine.png", "", x, y, "", true )
 end
