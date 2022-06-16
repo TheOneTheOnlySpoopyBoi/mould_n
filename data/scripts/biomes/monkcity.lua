91c02491e595f1e486e6c27a8037aa059be9b849
Mir Sole <zburn101@gmail.com>
2022-06-16 20:35:21 +1000

    jason

--- data/scripts/biomes/monkcity.lua
+++ data/scripts/biomes/monkcity.lua
@@ -1,9 +1,11 @@
 CHEST_LEVEL = 3
 dofile_once("data/scripts/director_helpers.lua")
+dofile_once("data/scripts/director_helpers_design.lua")
 dofile_once("data/scripts/biome_scripts.lua")
 
 RegisterSpawnFunction( 0xffffeedd, "init" )
 RegisterSpawnFunction( 0xff528900, "spawn_monks" ) --0xffffdd35
+RegisterSpawnFunction( 0xff400080, "spawn_miniboss" ) --why no work this is so sad
 
 
 g_monks =
@@ -18,8 +20,24 @@ g_monks =
 	},
 }
 
+g_miniboss =
+{
+	total_prob = 0,
+
+	{
+		prob   		= 1,
+		min_count	= 1,
+		max_count	= 1,    
+		entity 	= "data/entities/animals/jason.xml"
+	},
+}
+
 function spawn_monks(x, y)
 	spawn(g_monks,x,y)
+end
+
+function spawn_miniboss(x, y)
+	spawn(g_miniboss,x,y)
 end
 
 function init( x, y, w, h )
