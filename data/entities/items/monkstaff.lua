741c6aa604c73d90eac68bfd34b01d4548171372
Mir Sole <zburn101@gmail.com>
2022-06-25 18:47:29 +1000

    everything works :thumbsup:

--- data/entities/items/monkstaff.lua
+++ data/entities/items/monkstaff.lua
@@ -4,4 +4,4 @@ dofile("data/scripts/gun/procedural/gun_action_utils.lua")
 local entity_id = GetUpdatedEntityID()
 
 AddGunActionPermanent( entity_id, "MONKSTAFF_MELEE" ) --monkstaff melee passive
-AddGunActionPermanent( entity_id, "LASER" ) --monkstaff projectile
+AddGunActionPermanent( entity_id, "MONKSTAFF_PROJ" ) --monkstaff projectile
